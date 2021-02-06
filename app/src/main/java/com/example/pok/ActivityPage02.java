6da882b058cc3ffcd44829ba382854b12f5990a5
mongmang <mongmang.vak@gmail.com>
2021-02-06 21:09:50 +0700

    chang database

--- app/src/main/java/com/example/pok/ActivityPage02.java
+++ app/src/main/java/com/example/pok/ActivityPage02.java
@@ -2,19 +2,249 @@ package com.example.pok;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Switch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.pok.model.DeviceModel;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class ActivityPage02 extends AppCompatActivity
 
-{
+public class ActivityPage02 extends AppCompatActivity {
+
+    Switch F1F2SW_G;
+    Switch F1SW_A;
+    Switch F1SW_B;
+    Switch F1SW_C;
+    Switch F1SW_D;
+    Switch F1SW_E;
+    Switch F1SW_F;
+    Switch F1SW_G;
+    Switch F1SW_H;
+    Switch F1SW_I;
+    Switch F1SW_J;
+    Switch F1SW_K;
+    Switch F1SW_L;
+    Switch F2SW_A;
+    Switch F2SW_B;
+    Switch F2SW_C;
+    Switch F2SW_D;
+    Switch F2SW_E;
+    Switch F2SW_F;
+    Switch F2SW_H;
+    Switch F2SW_I;
+    Switch F2SW_J;
+    Switch F2SW_K;
+    Switch F2SW_L;
+    Switch F2SW_M;
+    Switch F2SW_N;
+    Switch F2SW_O;
+    Switch F2SW_P;
+
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference reference = database.getReference();
+    DeviceModel deviceModel;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_page02);
+        deviceModel = new DeviceModel();
+        findIdFormView();
+        getValeFormDatabase();
 
 
     }
+
+    private void findIdFormView() {
+        F1SW_A = findViewById(R.id.f1sw_a);
+        F1SW_B = findViewById(R.id.f1sw_b);
+        F1SW_C = findViewById(R.id.f1sw_c);
+        F1SW_D = findViewById(R.id.f1sw_d);
+        F1SW_E = findViewById(R.id.f1sw_e);
+        F1SW_F = findViewById(R.id.f1sw_f);
+        F1SW_G = findViewById(R.id.f1sw_g);
+        F1SW_H = findViewById(R.id.f1sw_h);
+        F1SW_I = findViewById(R.id.f1sw_i);
+        F1SW_J = findViewById(R.id.f1sw_j);
+        F1SW_K = findViewById(R.id.f1sw_k);
+        F1SW_L = findViewById(R.id.f1sw_l);
+
+    }
+
+    private void getValeFormDatabase() {
+        reference.child("device").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                deviceModel = dataSnapshot.getValue(DeviceModel.class);
+                Log.d("database", String.valueOf(deviceModel.getF1SW_A()));
+                setValueSwitchFomdata();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("database", databaseError.toString());
+            }
+        });
+    }
+
+    private void setValueSwitchFomdata() {
+        F1SW_A.setChecked(deviceModel.getF1SW_A() == 1);
+        F1SW_B.setChecked(deviceModel.getF1SW_B() == 1);
+        F1SW_C.setChecked(deviceModel.getF1SW_C() == 1);
+        F1SW_D.setChecked(deviceModel.getF1SW_D() == 1);
+        F1SW_E.setChecked(deviceModel.getF1SW_E() == 1);
+        F1SW_F.setChecked(deviceModel.getF1SW_F() == 1);
+        F1SW_G.setChecked(deviceModel.getF1SW_G() == 1);
+        F1SW_H.setChecked(deviceModel.getF1SW_H() == 1);
+        F1SW_I.setChecked(deviceModel.getF1SW_I() == 1);
+        F1SW_J.setChecked(deviceModel.getF1SW_J() == 1);
+        F1SW_K.setChecked(deviceModel.getF1SW_K() == 1);
+        F1SW_L.setChecked(deviceModel.getF1SW_L() == 1);
+    }
+
+    //รับค่าจากวิว
+    public void switchClick(View v) {
+        switch (v.getId()) {
+            case R.id.f1sw_a:
+                if (deviceModel.getF1SW_A() == 1) {
+                    deviceModel.setF1SW_A(0);
+                } else {
+                    deviceModel.setF1SW_A(1);
+                }
+                setValueToDatabase(deviceModel);
+
+                break;
+            case R.id.f1sw_b:
+                if (deviceModel.getF1SW_B() == 1) {
+                    deviceModel.setF1SW_B(0);
+                } else {
+                    deviceModel.setF1SW_B(1);
+                }
+                setValueToDatabase(deviceModel);
+
+                break;
+            case R.id.f1sw_c:
+                if (deviceModel.getF1SW_C() == 1) {
+                    deviceModel.setF1SW_C(0);
+                } else {
+                    deviceModel.setF1SW_C(1);
+                }
+                setValueToDatabase(deviceModel);
+
+                break;
+            case R.id.f1sw_d:
+                if (deviceModel.getF1SW_D() == 1) {
+                    deviceModel.setF1SW_D(0);
+                } else {
+                    deviceModel.setF1SW_D(1);
+                }
+                setValueToDatabase(deviceModel);
+
+                break;
+            case R.id.f1sw_e:
+                if (deviceModel.getF1SW_E() == 1) {
+                    deviceModel.setF1SW_E(0);
+                } else {
+                    deviceModel.setF1SW_E(1);
+                }
+                setValueToDatabase(deviceModel);
+
+                break;
+            case R.id.f1sw_f:
+                if (deviceModel.getF1SW_F() == 1) {
+                    deviceModel.setF1SW_F(0);
+                } else {
+                    deviceModel.setF1SW_F(1);
+                }
+                setValueToDatabase(deviceModel);
+
+                break;
+            case R.id.f1sw_g:
+                if (deviceModel.getF1SW_G() == 1) {
+                    deviceModel.setF1SW_G(0);
+                } else {
+                    deviceModel.setF1SW_G(1);
+                }
+                setValueToDatabase(deviceModel);
+
+                break;
+            case R.id.f1sw_h:
+                if (deviceModel.getF1SW_H() == 1) {
+                    deviceModel.setF1SW_H(0);
+                } else {
+                    deviceModel.setF1SW_H(1);
+                }
+                setValueToDatabase(deviceModel);
+
+                break;
+            case R.id.f1sw_i:
+                if (deviceModel.getF1SW_I() == 1) {
+                    deviceModel.setF1SW_I(0);
+                } else {
+                    deviceModel.setF1SW_I(1);
+                }
+                setValueToDatabase(deviceModel);
+
+                break;
+            case R.id.f1sw_j:
+                if (deviceModel.getF1SW_J() == 1) {
+                    deviceModel.setF1SW_J(0);
+                } else {
+                    deviceModel.setF1SW_J(1);
+                }
+                setValueToDatabase(deviceModel);
+
+                break;
+            case R.id.f1sw_k:
+                if (deviceModel.getF1SW_K() == 1) {
+                    deviceModel.setF1SW_K(0);
+                } else {
+                    deviceModel.setF1SW_K(1);
+                }
+                setValueToDatabase(deviceModel);
+
+                break;
+            case R.id.f1sw_l:
+                if (deviceModel.getF1SW_L() == 1) {
+                    deviceModel.setF1SW_L(0);
+                } else {
+                    deviceModel.setF1SW_L(1);
+                }
+                setValueToDatabase(deviceModel);
+
+                break;
+            default:
+                Log.d("switchClick", "defal");
+                break;
+        }
+
+    }
+
+    private void setValueToDatabase(DeviceModel model) {
+        reference.child("device").setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d("uploadData", "Complete");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("uploadData", "Fail");
+            }
+        });
+    }
+
+
 }
---
+++ app/src/main/java/com/example/pok/model/DeviceModel.java
@@ -0,0 +1,300 @@
+package com.example.pok.model;
+
+public class DeviceModel {
+   long F1F2SW_G;
+    long F1SW_A;
+    long  F1SW_B;
+    long  F1SW_C;
+    long  F1SW_D;
+    long  F1SW_E;
+    long F1SW_F;
+    long F1SW_G;
+    long F1SW_H;
+    long  F1SW_I;
+    long  F1SW_J;
+    long F1SW_K;
+    long F1SW_L;
+    long F2SW_A;
+    long F2SW_B;
+    long F2SW_C;
+    long F2SW_D;
+    long  F2SW_E;
+    long  F2SW_F;
+    long  F2SW_H;
+    long F2SW_I;
+    long F2SW_J;
+    long F2SW_K;
+    long F2SW_L;
+    long  F2SW_M;
+    long F2SW_N;
+    long  F2SW_O;
+    long  F2SW_P;
+    String  LED1;
+
+    public DeviceModel() {
+    }
+
+    public DeviceModel(long f1F2SW_G, long f1SW_A, long f1SW_B, long f1SW_C, long f1SW_D, long f1SW_E, long f1SW_F, long f1SW_G, long f1SW_H, long f1SW_I, long f1SW_J, long f1SW_K, long f1SW_L, long f2SW_A, long f2SW_B, long f2SW_C, long f2SW_D, long f2SW_E, long f2SW_F, long f2SW_H, long f2SW_I, long f2SW_J, long f2SW_K, long f2SW_L, long f2SW_M, long f2SW_N, long f2SW_O, long f2SW_P, String LED1) {
+        F1F2SW_G = f1F2SW_G;
+        F1SW_A = f1SW_A;
+        F1SW_B = f1SW_B;
+        F1SW_C = f1SW_C;
+        F1SW_D = f1SW_D;
+        F1SW_E = f1SW_E;
+        F1SW_F = f1SW_F;
+        F1SW_G = f1SW_G;
+        F1SW_H = f1SW_H;
+        F1SW_I = f1SW_I;
+        F1SW_J = f1SW_J;
+        F1SW_K = f1SW_K;
+        F1SW_L = f1SW_L;
+        F2SW_A = f2SW_A;
+        F2SW_B = f2SW_B;
+        F2SW_C = f2SW_C;
+        F2SW_D = f2SW_D;
+        F2SW_E = f2SW_E;
+        F2SW_F = f2SW_F;
+        F2SW_H = f2SW_H;
+        F2SW_I = f2SW_I;
+        F2SW_J = f2SW_J;
+        F2SW_K = f2SW_K;
+        F2SW_L = f2SW_L;
+        F2SW_M = f2SW_M;
+        F2SW_N = f2SW_N;
+        F2SW_O = f2SW_O;
+        F2SW_P = f2SW_P;
+        this.LED1 = LED1;
+    }
+
+    public long getF1F2SW_G() {
+        return F1F2SW_G;
+    }
+
+    public void setF1F2SW_G(long f1F2SW_G) {
+        F1F2SW_G = f1F2SW_G;
+    }
+
+    public long getF1SW_A() {
+        return F1SW_A;
+    }
+
+    public void setF1SW_A(long f1SW_A) {
+        F1SW_A = f1SW_A;
+    }
+
+    public long getF1SW_B() {
+        return F1SW_B;
+    }
+
+    public void setF1SW_B(long f1SW_B) {
+        F1SW_B = f1SW_B;
+    }
+
+    public long getF1SW_C() {
+        return F1SW_C;
+    }
+
+    public void setF1SW_C(long f1SW_C) {
+        F1SW_C = f1SW_C;
+    }
+
+    public long getF1SW_D() {
+        return F1SW_D;
+    }
+
+    public void setF1SW_D(long f1SW_D) {
+        F1SW_D = f1SW_D;
+    }
+
+    public long getF1SW_E() {
+        return F1SW_E;
+    }
+
+    public void setF1SW_E(long f1SW_E) {
+        F1SW_E = f1SW_E;
+    }
+
+    public long getF1SW_F() {
+        return F1SW_F;
+    }
+
+    public void setF1SW_F(long f1SW_F) {
+        F1SW_F = f1SW_F;
+    }
+
+    public long getF1SW_G() {
+        return F1SW_G;
+    }
+
+    public void setF1SW_G(long f1SW_G) {
+        F1SW_G = f1SW_G;
+    }
+
+    public long getF1SW_H() {
+        return F1SW_H;
+    }
+
+    public void setF1SW_H(long f1SW_H) {
+        F1SW_H = f1SW_H;
+    }
+
+    public long getF1SW_I() {
+        return F1SW_I;
+    }
+
+    public void setF1SW_I(long f1SW_I) {
+        F1SW_I = f1SW_I;
+    }
+
+    public long getF1SW_J() {
+        return F1SW_J;
+    }
+
+    public void setF1SW_J(long f1SW_J) {
+        F1SW_J = f1SW_J;
+    }
+
+    public long getF1SW_K() {
+        return F1SW_K;
+    }
+
+    public void setF1SW_K(long f1SW_K) {
+        F1SW_K = f1SW_K;
+    }
+
+    public long getF1SW_L() {
+        return F1SW_L;
+    }
+
+    public void setF1SW_L(long f1SW_L) {
+        F1SW_L = f1SW_L;
+    }
+
+    public long getF2SW_A() {
+        return F2SW_A;
+    }
+
+    public void setF2SW_A(long f2SW_A) {
+        F2SW_A = f2SW_A;
+    }
+
+    public long getF2SW_B() {
+        return F2SW_B;
+    }
+
+    public void setF2SW_B(long f2SW_B) {
+        F2SW_B = f2SW_B;
+    }
+
+    public long getF2SW_C() {
+        return F2SW_C;
+    }
+
+    public void setF2SW_C(long f2SW_C) {
+        F2SW_C = f2SW_C;
+    }
+
+    public long getF2SW_D() {
+        return F2SW_D;
+    }
+
+    public void setF2SW_D(long f2SW_D) {
+        F2SW_D = f2SW_D;
+    }
+
+    public long getF2SW_E() {
+        return F2SW_E;
+    }
+
+    public void setF2SW_E(long f2SW_E) {
+        F2SW_E = f2SW_E;
+    }
+
+    public long getF2SW_F() {
+        return F2SW_F;
+    }
+
+    public void setF2SW_F(long f2SW_F) {
+        F2SW_F = f2SW_F;
+    }
+
+    public long getF2SW_H() {
+        return F2SW_H;
+    }
+
+    public void setF2SW_H(long f2SW_H) {
+        F2SW_H = f2SW_H;
+    }
+
+    public long getF2SW_I() {
+        return F2SW_I;
+    }
+
+    public void setF2SW_I(long f2SW_I) {
+        F2SW_I = f2SW_I;
+    }
+
+    public long getF2SW_J() {
+        return F2SW_J;
+    }
+
+    public void setF2SW_J(long f2SW_J) {
+        F2SW_J = f2SW_J;
+    }
+
+    public long getF2SW_K() {
+        return F2SW_K;
+    }
+
+    public void setF2SW_K(long f2SW_K) {
+        F2SW_K = f2SW_K;
+    }
+
+    public long getF2SW_L() {
+        return F2SW_L;
+    }
+
+    public void setF2SW_L(long f2SW_L) {
+        F2SW_L = f2SW_L;
+    }
+
+    public long getF2SW_M() {
+        return F2SW_M;
+    }
+
+    public void setF2SW_M(long f2SW_M) {
+        F2SW_M = f2SW_M;
+    }
+
+    public long getF2SW_N() {
+        return F2SW_N;
+    }
+
+    public void setF2SW_N(long f2SW_N) {
+        F2SW_N = f2SW_N;
+    }
+
+    public long getF2SW_O() {
+        return F2SW_O;
+    }
+
+    public void setF2SW_O(long f2SW_O) {
+        F2SW_O = f2SW_O;
+    }
+
+    public long getF2SW_P() {
+        return F2SW_P;
+    }
+
+    public void setF2SW_P(long f2SW_P) {
+        F2SW_P = f2SW_P;
+    }
+
+    public String getLED1() {
+        return LED1;
+    }
+
+    public void setLED1(String LED1) {
+        this.LED1 = LED1;
+    }
+}
